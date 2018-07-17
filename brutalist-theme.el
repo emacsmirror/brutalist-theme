bca81f66b951cadc6d747410c9ebcab0db24b822
Gergely Nagy <emacs@gergo.csillger.hu>
2018-07-17 16:28:09 +0200

    Move all of the colors into the surrounding let
    While there, also change a few of them, so we have less colors overall.
    
    Signed-off-by: Gergely Nagy <emacs@gergo.csillger.hu>

--- brutalist-theme.el
+++ brutalist-theme.el
@@ -43,12 +43,31 @@
 
 (let ((fg "#111111")
       (fg-table "#222291")
+      (fg-dim "dim gray")
+      (fg-grey "grey70")
       (bg "#fffff8")
       (bg-light "#ddddd8")
       (fg-light "#ddddd8")
       (bg-highlight "#FFF1AA")
       (bg-highlight-2 "LightCyan")
-      (bg-highlight-3 "LightGreen"))
+      (bg-highlight-3 "LightGreen")
+      (bg-highlight-dim "#eeeee8")
+
+      (diff-added "#e9ffe9")
+      (diff-added-highlight "#a4f4a3")
+      (diff-removed "#ffecec")
+      (diff-removed-highlight "#f9cbca")
+
+      (powerline1 "grey22")
+      (powerline2 "grey40")
+
+      (link "blue")
+      (string "red")
+      (documentation "dark green")
+      (cursor "white smoke")
+
+      (paren-match "blue")
+      (paren-mismatch "red"))
 
   (custom-theme-set-faces
    'brutalist
@@ -56,24 +75,24 @@
    ;; generic stuff
    `(default ((t (:background ,bg :foreground ,fg :height 225))))
    `(button ((t (:foreground ,fg :underline t))))
-   `(cursor ((t (:background ,fg :foreground "white smoke"))))
+   `(cursor ((t (:background ,fg :foreground ,cursor))))
    `(custom-variable-tag ((t (:foreground ,fg :weight bold))))
    `(default-italic ((t (:italic t))))
    `(font-latex-bold-face ((t (:foreground ,fg))))
    `(font-latex-italic-face ((t (:foreground ,fg :slant italic))))
    `(font-latex-match-reference-keywords ((t (:foreground ,fg))))
    `(font-latex-match-variable-keywords ((t (:foreground ,fg))))
-   `(font-latex-string-face ((t (:foreground "#a9a9a9"))))
+   `(font-latex-string-face ((t (:foreground ,string))))
    `(font-lock-builtin-face ((t (:background ,bg :foreground ,fg))))
-   `(font-lock-comment-delimiter-face ((t (:foreground "#808080"))))
-   `(font-lock-comment-face ((t (:foreground "dim gray" :weight normal))))
+   `(font-lock-comment-face ((t (:foreground ,fg-dim :weight normal))))
+   `(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face))))
    `(font-lock-constant-face ((t (:foreground ,fg))))
-   `(font-lock-doc-face ((t (:foreground "dark green" :weight normal))))
+   `(font-lock-doc-face ((t (:foreground ,documentation :weight normal))))
    `(font-lock-function-name-face ((t (:foreground ,fg))))
    `(font-lock-keyword-face ((t (:foreground ,fg :weight bold))))
    `(font-lock-preprocessor-face ((t (:foreground ,fg :slant italic))))
    `(font-lock-reference-face ((t (:foreground ,fg))))
-   `(font-lock-string-face ((t (:foreground "red"))))
+   `(font-lock-string-face ((t (:foreground ,string))))
    `(font-lock-type-face ((t (:foreground ,fg :underline t))))
    `(font-lock-variable-name-face ((t (:foreground ,fg :underline nil))))
    `(font-lock-warning-face ((t (:foreground ,fg :weight bold))))
@@ -82,15 +101,15 @@
    `(ido-first-match ((t (:foreground ,fg))))
    `(ido-only-match ((t (:foreground ,fg))))
    `(ido-subdir ((t (:foreground ,fg))))
-   `(isearch ((t (:background "#eeeee8" :foreground ,fg))))
-   `(link ((t (:foreground "blue"))))
+   `(isearch ((t (:background ,bg-highlight-dim :foreground ,fg))))
+   `(link ((t (:foreground ,link))))
    `(minibuffer-prompt ((t (:foreground ,fg :weight bold))))
    `(mode-line ((t (:background ,bg-light :foreground ,fg :height 0.8))))
    `(mode-line-buffer ((t (:foreground ,fg :weight bold))))
    `(mode-line-inactive ((t (:background ,bg-light :foreground ,bg-light :height 0.8))))
    `(mode-line-minor-mode ((t (:weight ultra-light))))
    `(modeline ((t (:background ,bg :foreground ,fg :height 0.8))))
-   `(region ((t (:background "#eeeee8" :foreground ,fg))))
+   `(region ((t (:background ,bg-highlight-dim :foreground ,fg))))
    `(slime-repl-inputed-output-face ((t (:foreground ,fg))))
    `(whitespace-line ((t (:background ,bg-highlight-2 :foreground ,fg))))
 
@@ -123,8 +142,8 @@
    `(org-table ((t (:foreground ,fg-table))))
 
    ;; powerline
-   `(powerline-active1 ((t (:background "grey22" :foreground ,bg :inherit mode-line))))
-   `(powerline-active2 ((t (:background "grey40" :foreground ,bg :inherit mode-line))))
+   `(powerline-active1 ((t (:background ,powerline1 :foreground ,bg :inherit mode-line))))
+   `(powerline-active2 ((t (:background ,powerline2 :foreground ,bg :inherit mode-line))))
 
    ;; message-mode, notmuch and gnus
    `(gnus-header-content ((t (:foreground ,fg))))
@@ -136,8 +155,8 @@
    `(message-header-to ((t (:inherit message-header-other))))
    `(message-header-cc ((t (:inherit message-header-other))))
    `(message-header-subject ((t (:inherit message-header-other :foreground ,fg-table))))
-   `(message-mml ((t (:foreground "dim gray"))))
-   `(message-cited-text ((t (:foreground "dim gray"))))
+   `(message-mml ((t (:foreground ,fg-dim))))
+   `(message-cited-text ((t (:foreground ,fg-dim))))
    `(notmuch-message-summary-face ((t (:foreground ,fg))))
    `(notmuch-tree-match-author-face ((t (:foreground ,fg))))
    `(notmuch-tree-match-tag-face ((t (:foreground ,fg :weight normal))))
@@ -153,18 +172,20 @@
    `(magit-branch-remote ((t (:weight bold))))
 
    `(git-commit-comment-branch ((t (:inherit link))))
-   `(git-commit-known-pseudo-header ((t (:inherit font-lock-keyword-face :box (:line-width 1 :color "grey75") :weight normal))))
+   `(git-commit-known-pseudo-header ((t (:inherit font-lock-keyword-face
+                                                  :box (:line-width 1 :color ,fg-grey)
+                                                  :weight normal))))
    `(git-commit-summary ((t (:inherit font-lock-type-face :underline nil :weight bold))))
 
    ;; diff
-   `(diff-added ((t (:background "#e9ffe9"))))
-   `(diff-removed ((t (:background "#ffecec"))))
-   `(diff-refine-added ((t (:background "#a4f4a3"))))
-   `(diff-refine-removed ((t (:background "#f9cbca"))))
-   `(magit-diff-added-highlight ((t (:weight demibold :background "#e9ffe9"))))
-   `(magit-diff-added ((t (:background "#e9ffe9"))))
-   `(magit-diff-removed-highlight ((t (:weight demibold :background "#ffecec"))))
-   `(magit-diff-removed ((t (:background "#ffecec"))))
+   `(diff-added ((t (:background ,diff-added))))
+   `(diff-removed ((t (:background ,diff-removed))))
+   `(diff-refine-added ((t (:background ,diff-added-highlight))))
+   `(diff-refine-removed ((t (:background ,diff-removed-highlight))))
+   `(magit-diff-added-highlight ((t (:weight demibold :background ,diff-added))))
+   `(magit-diff-added ((t (:background ,diff-added))))
+   `(magit-diff-removed-highlight ((t (:weight demibold :background ,diff-removed))))
+   `(magit-diff-removed ((t (:background ,diff-removed))))
 
    ;; git-timemachine
    `(git-timemachine-minibuffer-author-face ((t (:inherit default))))
@@ -185,7 +206,7 @@
    `(dired-subtree-depth-4-face ((t (:inherit default))))
 
    ;; helm
-   `(helm-source-header ((t (:foreground ,fg :background "grey90" :weight bold))))
+   `(helm-source-header ((t (:foreground ,fg :background ,bg-light :weight bold))))
    `(helm-header ((t (:foreground ,fg))))
    `(helm-selection-line ((t (:inherit region :weight bold))))
    `(helm-selection ((t (:background ,bg-highlight))))
@@ -202,18 +223,18 @@
    `(company-tooltip-selection ((t (:background ,bg-highlight))))
 
    ;; parens - parenface
-   '(parenface-paren-face ((t (:foreground "gray70"))))
-   '(parenface-curly-face ((t (:foreground "gray70"))))
-   '(parenface-bracket-face ((t (:foreground "gray70"))))
+   '(parenface-paren-face ((t (:foreground ,fg-gray))))
+   '(parenface-curly-face ((t (:foreground ,fg-gray))))
+   '(parenface-bracket-face ((t (:foreground ,fg-gray))))
 
    ;; parens - paren-face
-   '(parenthesis ((t (:foreground "gray70"))))
+   '(parenthesis ((t (:foreground ,fg-gray))))
 
    ;; parens - other
-   `(sp-show-pair-match-face ((t (:foreground "black" :weight bold))))
-   `(sp-show-pair-mismatch-face ((t (:background "red" :foreground "black" :weight bold))))
-   `(show-paren-match ((t (:foreground "black" :weight bold))))
-   `(show-paren-mismatch ((t (:background "red" :foreground "black" :weight bold))))
+   `(sp-show-pair-match-face ((t (:foreground ,paren-match :weight bold))))
+   `(sp-show-pair-mismatch-face ((t (:background ,paren-mismatch :foreground ,paren-match :weight bold))))
+   `(show-paren-match ((t (:foreground ,paren-match :weight bold))))
+   `(show-paren-mismatch ((t (:background ,paren-mismatch :foreground ,paren-match :weight bold))))
 
    ;; js2
    `(js2-function-param ((t (:foreground ,fg))))
@@ -233,16 +254,16 @@
 
    ;; misc
    `(idle-highlight ((t (:background ,bg-highlight))))
-   `(yas-field-highlight-face ((t (:background "#eeeee8" :foreground ,fg))))
+   `(yas-field-highlight-face ((t (:background ,bg-highlight-dim :foreground ,fg))))
    `(eshell-prompt ((t (:foreground ,fg :weight bold))))
    `(cider-result-overlay-face ((t (:weight bold))))
 
    ;; evil-quickscope
-   `(evil-quickscope-first-face ((t (:foreground ,fg :background "#eeeee8"))))
+   `(evil-quickscope-first-face ((t (:foreground ,fg :background ,bg-highlight-dim))))
    `(evil-quickscope-second-face ((t (:foreground ,fg :background ,bg-highlight-3))))
 
    ;; evil-snipe
-   `(evil-snipe-first-match-face ((t (:foreground ,fg :background "#eeeee8"))))
+   `(evil-snipe-first-match-face ((t (:foreground ,fg :background ,bg-highlight-dim))))
    `(evil-snipe-matches-face ((t (:foreground ,fg :background ,bg-highlight-3))))
 
    ;; evil-goggles
